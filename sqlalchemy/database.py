aa4f1a3713853874c50855e42bb7295c5c4a302b
Andrewgl22 <ledermanmusic@gmail.com>
2025-12-31 13:35:01 -0500

    fixed database url to work local and remote

--- sqlalchemy/database.py
+++ sqlalchemy/database.py
@@ -1,9 +1,12 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://local:local222@localhost:5432/trading"
-
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://local:local222@localhost:5432/trading",  # fallback for local dev
+)
 engine = create_engine(DATABASE_URL, echo=True)  # echo prints SQL logs
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
